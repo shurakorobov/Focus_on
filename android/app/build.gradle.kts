b80d9036767ccbaf8044d09d1acc2b2c2945ef89
shurakorobov <shura.korobov@gmail.com>
2026-07-19 16:20:41 +0300

    Bump version до 1.2 (versionCode 3)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.focuson.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
